0d2ca05dcb3afb18395842a9aa93cca7886cacdb
Anushka Dasgupta <70762274+anushkadasgupta@users.noreply.github.com>
2024-03-18 13:18:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template("index.html")
+@app.route('/home')
+def red_home():
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
